32cbaf6b496bb9d9620c5e97597460ab31e96331
Ernest <ernestcichocki404@gmail.com>
2024-02-12 19:22:31 +0100

    area counting

--- app/src/main/java/com/mobile/areacounter/geometry/GeometryHelper.kt
+++ app/src/main/java/com/mobile/areacounter/geometry/GeometryHelper.kt
@@ -3,8 +3,11 @@ package com.mobile.areacounter.geometry
 import kotlin.math.acos
 import kotlin.math.pow
 import kotlin.math.sqrt
+import kotlin.random.Random
 
 object GeometryHelper {
+    val random = Random
+
     fun calculateAngle(firstVector: Vector, secondVector: Vector) : Double {
         val firstVectorCommonStart = Point(
             firstVector.finishingPoint.x - firstVector.startingPoint.x,
@@ -36,6 +39,30 @@ object GeometryHelper {
 
         return Pair(180.0 + alpha, 180.0 - alpha)
 
+    }
+
+    fun calculateMonteCarloArea(polygon: Polygon, probes : Int = 1000): Double {
+        val extremePoints = polygon.getExtremes()
+
+        var pointsInside = 0
+
+        for (index in 0..probes){
+            if (polygon.triangulate().filter {
+                    it.belongsToTriangle(
+                        Point(
+                            random.nextDouble(extremePoints.xMin, extremePoints.xMax),
+                            random.nextDouble(extremePoints.yMin, extremePoints.yMax)
+                        )
+                    )
+                }.toSet().isNotEmpty()){
+                pointsInside += 1
+            }
+        }
+        val squareX = extremePoints.xMax - extremePoints.xMin
+        val squareY = extremePoints.yMax - extremePoints.yMin
+        val squareArea = squareX * squareY
+
+        return squareArea * pointsInside / probes
     }
 
     private fun bendsToLeft(v1: Vector, v2: Vector): Boolean  {
